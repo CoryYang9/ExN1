50cc0ffa955e41ee4e8d14504e4293053f1f8a6f
GodofBiceps <coryyang@gmail.com>
2021-03-03 17:31:25 -0800

    test

--- index.js
+++ index.js
@@ -1,45 +1,64 @@
-// const cool = require('cool-ascii-faces')
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-//
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-//
-// express()
-//     .use(express.static(path.join(__dirname, 'public')))
-//     .set('views', path.join(__dirname, 'views'))
-//     .set('view engine', 'ejs')
-//     .get('/times', (req, res) => res.send(showTimes()))
-//     .get('/', (req, res) => res.render('pages/index'))
-//     .get('/db', async (req, res) => {
-//         try {
-//             const client = await pool.connect();
-//             const result = await client.query('SELECT * FROM test_table');
-//             const results = { 'results': (result) ? result.rows : null};
-//             res.render('pages/db', results );
-//             client.release();
-//         } catch (err) {
-//             console.error(err);
-//             res.send("Error " + err);
-//         }
-//     })
-//     .get('/cool', (req, res) => res.send(cool()))
-//     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-//
-// showTimes = () => {
-//     let result = '';
-//     const times = process.env.TIMES || 5;
-//     for (i = 0; i < times; i++) {
-//         result += i + ' ';
-//     }
-//     return result;
-// }
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/times', (req, res) => res.send(showTimes()))
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'results': (result) ? result.rows : null};
+            res.render('pages/db', results );
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
+    .get('/cool', (req, res) => res.send(cool()))
+    .get('/mongodb', function (request, response) {
+        mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+            if(err) throw err;
+            //get collection of routes
+            var Routes = db.collection('Test');
+            //get all Routes with frequency >=1
+            Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
+                if(err) throw err;
+
+                response.render('pages/mongodb', {results: docs});
+
+            });
+
+            //close connection when your app is terminating.
+            db.close(function (err) {
+                if(err) throw err;
+            });
+        });//end of connect
+    });//end app.get
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+showTimes = () => {
+    let result = '';
+    const times = process.env.TIMES || 5;
+    for (i = 0; i < times; i++) {
+        result += i + ' ';
+    }
+    return result;
+}
 
 //**************************************************************************
 //***** mongodb get all of the Routes in Routes collection where frequence>=1
