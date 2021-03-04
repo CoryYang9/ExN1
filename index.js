19851a1c1f5cb2de510fdad31955e42f5ac2255e
GodofBiceps <coryyang@gmail.com>
2021-03-03 17:27:09 -0800

    test

--- index.js
+++ index.js
@@ -45,11 +45,10 @@
 //***** mongodb get all of the Routes in Routes collection where frequence>=1
 //      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
 app.get('/mongodb', function (request, response) {
-
     mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
         if(err) throw err;
         //get collection of routes
-        var Routes = db.collection('Routes');
+        var Routes = db.collection('Test');
         //get all Routes with frequency >=1
         Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
             if(err) throw err;
